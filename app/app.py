1ec399d34f5f253e776c0f37c594030b89413377
Aymeric <contact@aymeric-mai.fr>
2024-05-04 20:33:15 +0200

    add setup endpoint

--- app/app.py
+++ app/app.py
@@ -1,88 +1,106 @@
 import json
 import falcon
-from utils import Upload, Rec, Preview
+from utils import Setup
+# from utils import Upload, Rec, Preview
 
-rec = Rec()
-preview = Preview()
-upload = Upload()
+setup = Setup()
+# rec = Rec()
+# preview = Preview()
+# upload = Upload()
 
-
-class RunRecSession(object):
+class SetupSimCam(object):
     def on_post(self, req, resp):
         """Handles POST requests"""
         data = json.load(req.stream)
-        name = data['name']
-        time_limit = data['time']
-        res = rec.rec_video(name, time_limit)
+        login = data['login']
+        password = data['password']
+        directory = data['directory']
+        res = setup.setup_simcam(login, password, directory)
         if res[0]:
-            resp.status = falcon.HTTP_200  # This is the default status
-            resp.text = json.dumps({"volunteer name": name})
-        else:
-            resp.status = falcon.HTTP_200  # This is the default status
-            resp.text = json.dumps({"status": res[1]})
-
-
-class StopRecSession(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        path = rec.unrec_video()
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.text = json.dumps({"path": path})
-
-
-class StatusRecSession(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        status = rec.status_rec()
-        if status:
-            info = rec.info_rec()
             resp.status = falcon.HTTP_200
-            resp.text = json.dumps(
-                {"pid": info[0], 'name': info[1], 'file': info[2], 'path': info[3], 'time': info[4], 'status': info[5]})
+            resp.text = json.dumps({"status": res[0], "message": res[1]})
         else:
-            resp.status = falcon.HTTP_200  # This is the default status
-            resp.text = json.dumps({"status": status})
+            resp.status = falcon.HTTP_500
+            resp.text = json.dumps({"status": res[0], "message": res[1]})
 
-
-class UploadFile(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        status = upload.upload_file()
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.text = json.dumps({"status": status})
-
-
-class RunPreview(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        status = preview.run_preview()
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.text = json.dumps({"status": status})
-
-
-class StopPreview(object):
-    def on_get(self, req, resp):
-        """Handles GET requests"""
-        status = preview.stop_preview()
-        resp.status = falcon.HTTP_200  # This is the default status
-        resp.text = json.dumps({"status": status})
+# class RunRecSession(object):
+#     def on_post(self, req, resp):
+#         """Handles POST requests"""
+#         data = json.load(req.stream)
+#         name = data['name']
+#         time_limit = data['time']
+#         res = rec.rec_video(name, time_limit)
+#         if res[0]:
+#             resp.status = falcon.HTTP_200  # This is the default status
+#             resp.text = json.dumps({"volunteer name": name})
+#         else:
+#             resp.status = falcon.HTTP_200  # This is the default status
+#             resp.text = json.dumps({"status": res[1]})
+#
+#
+# class StopRecSession(object):
+#     def on_get(self, req, resp):
+#         """Handles GET requests"""
+#         path = rec.unrec_video()
+#         resp.status = falcon.HTTP_200  # This is the default status
+#         resp.text = json.dumps({"path": path})
+#
+#
+# class StatusRecSession(object):
+#     def on_get(self, req, resp):
+#         """Handles GET requests"""
+#         status = rec.status_rec()
+#         if status:
+#             info = rec.info_rec()
+#             resp.status = falcon.HTTP_200
+#             resp.text = json.dumps(
+#                 {"pid": info[0], 'name': info[1], 'file': info[2], 'path': info[3], 'time': info[4], 'status': info[5]})
+#         else:
+#             resp.status = falcon.HTTP_200  # This is the default status
+#             resp.text = json.dumps({"status": status})
+#
+#
+# class UploadFile(object):
+#     def on_get(self, req, resp):
+#         """Handles GET requests"""
+#         status = upload.upload_file()
+#         resp.status = falcon.HTTP_200  # This is the default status
+#         resp.text = json.dumps({"status": status})
+#
+#
+# class RunPreview(object):
+#     def on_get(self, req, resp):
+#         """Handles GET requests"""
+#         status = preview.run_preview()
+#         resp.status = falcon.HTTP_200  # This is the default status
+#         resp.text = json.dumps({"status": status})
+#
+#
+# class StopPreview(object):
+#     def on_get(self, req, resp):
+#         """Handles GET requests"""
+#         status = preview.stop_preview()
+#         resp.status = falcon.HTTP_200  # This is the default status
+#         resp.text = json.dumps({"status": status})
 
 
 # falcon.API instances are callable WSGI apps
 app = falcon.App()
 
 # Resources are represented by long-lived class instances
-run_rec = RunRecSession()
-stop_rec = StopRecSession()
-status_rec = StatusRecSession()
-upload_file = UploadFile()
-run_preview = RunPreview()
-stop_preview = StopPreview()
+set_setup = SetupSimCam()
+# run_rec = RunRecSession()
+# stop_rec = StopRecSession()
+# status_rec = StatusRecSession()
+# upload_file = UploadFile()
+# run_preview = RunPreview()
+# stop_preview = StopPreview()
 
 # things will handle all requests to the '/things' URL path
-app.add_route('/rec', run_rec)
-app.add_route('/unrec', stop_rec)
-app.add_route('/status', status_rec)
-app.add_route('/upload', upload_file)
-app.add_route('/preview', run_preview)
-app.add_route('/unpreview', stop_preview)
+app.add_route('/setup', set_setup)
+# app.add_route('/rec', run_rec)
+# app.add_route('/unrec', stop_rec)
+# app.add_route('/status', status_rec)
+# app.add_route('/upload', upload_file)
+# app.add_route('/preview', run_preview)
+# app.add_route('/unpreview', stop_preview)
--- app/utils.py
+++ app/utils.py
@@ -5,126 +5,173 @@ import subprocess
 from pathlib import Path
 import nextcloud_client
 from dotenv import load_dotenv
+import sqlite3
 
-class Rec:
+class Setup:
     def __init__(self):
         self.status = False
-        self.pid = None
-        self.file_source = None
-        self.file_name = None
-        self.name = None
-        self.time = None
-        self.time_limit = None
+        self.message = None
+        self.login = None
+        self.password = None
+        self.directory = None
 
-    def rec_video(self, name, time_limit):
-        # Déclaration de variable
-        self.time = datetime.datetime.now().timestamp()
-        timestamp = datetime.datetime.now()
-        jour = timestamp.strftime('%d-%m-%y_%Hh%M')
-
-        # Création dossier
-        path = Path('/simcam/data/video/')
-        path.mkdir(parents=True, exist_ok=True)
-
-        # Nom fichier
-        self.name = name.replace(" ", "_")
-        self.file_name = self.name + '_' + jour + '-quality.mp4'
-        self.file_source = '/simcam/data/video/' + self.file_name
-
-        # Set time limit Rec
-        self.time_limit = time_limit
-
-        # Démarrage VLC
-        cmdbase = 'libcamera-vid --rotation 180 --nopreview -t ' + self.time_limit + ' --codec libav -o ' + self.file_source + ' --framerate 25 --width 1920 --height 1080 --profile high --level 4.2 --bitrate 2500000 -n --libav-audio --audio-source alsa --audio-device default --audio-bitrate 128000'
-        process = subprocess.Popen(cmdbase, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
-                                   preexec_fn=os.setsid)
-        # Stockage du PID
-        self.pid = os.getpgid(process.pid)
-
-        # Status d'enregistrement
+    def setup_simcam(self, login, password, directory):
+        self.login = login
+        self.password = password
+        self.directory = directory
         self.status = True
-
-        return self.status, self.pid
-
-    def unrec_video(self):
-        load_dotenv()
-
-        login = os.getenv('NEXTCLOUD_LOGIN')
-        password = os.getenv('NEXTCLOUD_PASSWORD')
-        url_nextcloud = os.getenv('NEXTCLOUD_URL')
-        datetime.datetime.now().timestamp()
-        timestamp = datetime.datetime.now()
-        annee = timestamp.strftime('%Y')
-        semaine = timestamp.strftime('%V le %m.%y')
+        self.message = "Configuration saved successfully"
 
         try:
-            os.killpg(self.pid, signal.SIGINT)
-        except ProcessLookupError:
-            pass
+            dir = Path('../db')
+            dir.mkdir(parents=True, exist_ok=True)
+            filename = Path('../db/simdb.db')
+            filename.touch(exist_ok=True)
 
-        # Use owncloud library for create dir of file mp4
-        try:
-            nc = nextcloud_client.Client(url_nextcloud)
-            nc.login(login, password)
-
-            try:
-                nc.mkdir('Simon/Vidéos-Simon/' + annee + '')
-            except:
-                print("Directory Exist")
-
-            try:
-                nc.mkdir('Simon/Vidéos-Simon/' + annee + '/Semaine-' + semaine + '')
-            except:
-                print("Directory Exist")
-
-        except:
-            pass
-
-        # Pour une utilisation de la library python
-        # oc.drop_file('/home/aymeric/Codage/AEVE-REC-API/app/test.txt')
-
-        # Changement status de l'enregistrement
-        self.status = False
-
-        # Supprimer le fichier vidéo temporaire
-        # if os.path.exists(file_source):
-        #    os.remove(file_source)
-
-        return self.file_source
-
-    def status_rec(self):
-        return self.status
-
-    def info_rec(self):
-        return self.pid, self.name, self.file_name, self.file_source, self.time, self.status
+            con = sqlite3.connect("../db/simdb.db")
+            cur = con.cursor()
+            cur.execute('''CREATE TABLE IF NOT EXISTS setup(
+                id INTEGER PRIMARY KEY AUTOINCREMENT UNIQUE,
+                login TEXT,
+                password TEXT,
+                directory TEXT
+            )''')
+            cur.execute("SELECT id FROM setup")
+            data = cur.fetchone()
+            if data is not None:
+                print('Exist')
+                cur.execute("DELETE FROM setup")
+                con.commit()
+            donnees = (login, password, directory)
+            cur.execute("INSERT INTO setup (login, password, directory) VALUES (?, ?, ?)", donnees)
+            con.commit()
+            con.close()
+        except sqlite3.Error as error:
+            print("Failed to insert data into sqlite table", error)
+            self.message = "Failed to insert data into sqlite table" + error
+            self.status = False
+        return self.status, self.message
 
 
-class Upload:
-    def upload_file(self):
-        load_dotenv()
-        login = str(os.getenv('NEXTCLOUD_LOGIN'))
-        password = str(os.getenv('NEXTCLOUD_PASSWORD'))
-        cmd_upload = 'login="' + login + '" password="' + password + '" bash /simcam/cron/AEVE-REC_Cron.bash >> /simcam/log/AEVE-REC_Cron.txt'
-        subprocess.Popen(cmd_upload, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
-        return True
-
-
-class Preview:
-    def __init__(self):
-        self.pid = None
-
-    def run_preview(self):
-        cmdbase = 'libcamera-vid -t 600000 --rotation 180 --width 1920 --height 1080 --codec h264 --inline --listen -o tcp://0.0.0.0:8888'
-        process = subprocess.Popen(cmdbase, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
-                                   preexec_fn=os.setsid)
-        self.pid = os.getpgid(process.pid)
-
-        return True
-
-    def stop_preview(self):
-        try:
-            os.killpg(self.pid, signal.SIGINT)
-        except ProcessLookupError:
-            pass
-
-        return True
+# class Rec:
+#     def __init__(self):
+#         self.status = False
+#         self.pid = None
+#         self.file_source = None
+#         self.file_name = None
+#         self.name = None
+#         self.time = None
+#         self.time_limit = None
+#
+#     def rec_video(self, name, time_limit):
+#         # Déclaration de variable
+#         self.time = datetime.datetime.now().timestamp()
+#         timestamp = datetime.datetime.now()
+#         jour = timestamp.strftime('%d-%m-%y_%Hh%M')
+#
+#         # Création dossier
+#         path = Path('/simcam/data/video/')
+#         path.mkdir(parents=True, exist_ok=True)
+#
+#         # Nom fichier
+#         self.name = name.replace(" ", "_")
+#         self.file_name = self.name + '_' + jour + '-quality.mp4'
+#         self.file_source = '/simcam/data/video/' + self.file_name
+#
+#         # Set time limit Rec
+#         self.time_limit = time_limit
+#
+#         # Démarrage VLC
+#         cmdbase = 'libcamera-vid --rotation 180 --nopreview -t ' + self.time_limit + ' --codec libav -o ' + self.file_source + ' --framerate 25 --width 1920 --height 1080 --profile high --level 4.2 --bitrate 2500000 -n --libav-audio --audio-source alsa --audio-device default --audio-bitrate 128000'
+#         process = subprocess.Popen(cmdbase, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
+#                                    preexec_fn=os.setsid)
+#         # Stockage du PID
+#         self.pid = os.getpgid(process.pid)
+#
+#         # Status d'enregistrement
+#         self.status = True
+#
+#         return self.status, self.pid
+#
+#     def unrec_video(self):
+#         load_dotenv()
+#
+#         login = os.getenv('NEXTCLOUD_LOGIN')
+#         password = os.getenv('NEXTCLOUD_PASSWORD')
+#         url_nextcloud = os.getenv('NEXTCLOUD_URL')
+#         datetime.datetime.now().timestamp()
+#         timestamp = datetime.datetime.now()
+#         annee = timestamp.strftime('%Y')
+#         semaine = timestamp.strftime('%V le %m.%y')
+#
+#         try:
+#             os.killpg(self.pid, signal.SIGINT)
+#         except ProcessLookupError:
+#             pass
+#
+#         # Use owncloud library for create dir of file mp4
+#         try:
+#             nc = nextcloud_client.Client(url_nextcloud)
+#             nc.login(login, password)
+#
+#             try:
+#                 nc.mkdir('Simon/Vidéos-Simon/' + annee + '')
+#             except:
+#                 print("Directory Exist")
+#
+#             try:
+#                 nc.mkdir('Simon/Vidéos-Simon/' + annee + '/Semaine-' + semaine + '')
+#             except:
+#                 print("Directory Exist")
+#
+#         except:
+#             pass
+#
+#         # Pour une utilisation de la library python
+#         # oc.drop_file('/home/aymeric/Codage/AEVE-REC-API/app/test.txt')
+#
+#         # Changement status de l'enregistrement
+#         self.status = False
+#
+#         # Supprimer le fichier vidéo temporaire
+#         # if os.path.exists(file_source):
+#         #    os.remove(file_source)
+#
+#         return self.file_source
+#
+#     def status_rec(self):
+#         return self.status
+#
+#     def info_rec(self):
+#         return self.pid, self.name, self.file_name, self.file_source, self.time, self.status
+#
+#
+# class Upload:
+#     def upload_file(self):
+#         load_dotenv()
+#         login = str(os.getenv('NEXTCLOUD_LOGIN'))
+#         password = str(os.getenv('NEXTCLOUD_PASSWORD'))
+#         cmd_upload = 'login="' + login + '" password="' + password + '" bash /simcam/cron/AEVE-REC_Cron.bash >> /simcam/log/AEVE-REC_Cron.txt'
+#         subprocess.Popen(cmd_upload, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True)
+#         return True
+#
+#
+# class Preview:
+#     def __init__(self):
+#         self.pid = None
+#
+#     def run_preview(self):
+#         cmdbase = 'libcamera-vid -t 600000 --rotation 180 --width 1920 --height 1080 --codec h264 --inline --listen -o tcp://0.0.0.0:8888'
+#         process = subprocess.Popen(cmdbase, stdin=subprocess.PIPE, stdout=subprocess.PIPE, shell=True,
+#                                    preexec_fn=os.setsid)
+#         self.pid = os.getpgid(process.pid)
+#
+#         return True
+#
+#     def stop_preview(self):
+#         try:
+#             os.killpg(self.pid, signal.SIGINT)
+#         except ProcessLookupError:
+#             pass
+#
+#         return True
